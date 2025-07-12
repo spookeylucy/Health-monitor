53b9a654a13f65e7999c7aa8316b1e69bb068ffa
spookeylucy <lucyannmwangi141@gmail.com>
2025-07-12 22:05:36 +0300

    updated backend url

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
 
     try {
       // Replace with your deployed backend URL
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://health-monitor-4.onrender.com/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
